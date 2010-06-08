dd38864d5624de6594a6a8946e13d462417a4925
Wincent Balin <wincent.balin@gmail.com>
2010-06-08 04:33:15 +0200

    Import of the project

---
+++ src/AboutForm.java
@@ -0,0 +1,26 @@
+import javax.microedition.lcdui.Form;
+import javax.microedition.lcdui.TextField;
+
+/**
+ * Formular which shows information about the application.
+ *
+ * @author Wincent Balin
+ */
+
+public class AboutForm extends Form
+{
+    public static final String aboutTitle = "About this program...";
+    public static final String aboutText = "About this program...";
+
+    AboutForm()
+    {
+        // Create formular with appropriate title
+        super(aboutTitle);
+
+        // Add text field with description of the program
+        append(new TextField("About",
+                             aboutText,
+                             aboutText.length(),
+                             TextField.UNEDITABLE));
+    }
+}
---
+++ src/Piano.java
@@ -0,0 +1,113 @@
+import javax.microedition.lcdui.Canvas;
+import javax.microedition.lcdui.Command;
+import javax.microedition.lcdui.CommandListener;
+import javax.microedition.lcdui.Display;
+import javax.microedition.lcdui.Displayable;
+import javax.microedition.lcdui.Form;
+import javax.microedition.midlet.MIDlet;
+
+/**
+ * Main class of the Piano MIDlet.
+ *
+ * Originally taken as an idea from the book
+ * "Wireless Java Developing with J2ME, Second Edition" by Jonathan Knudsen,
+ * published by Apress, ISBN 1590590775
+ * 
+ * @author Wincent Balin
+ * @version 1
+ */
+public class Piano extends MIDlet implements CommandListener
+{
+    public static final String COMMAND_ABOUT = "About";
+    public static final String COMMAND_EXIT = "Exit";
+    public static final String COMMAND_BACK = "Back";
+
+    private Command about;
+    private Command exit;
+    private Command back;
+
+    private Display display;
+
+    private Canvas pianoCanvas;
+    private Form aboutForm;
+
+    /**
+     * Constructor of the MIDlet.
+     */
+    public Piano()
+    {
+        // Initialize commands
+        about = new Command(COMMAND_ABOUT, Command.SCREEN, 1);
+        exit = new Command(COMMAND_EXIT, Command.EXIT, 0);
+        back = new Command(COMMAND_BACK, Command.BACK, 0);
+    }
+
+    /**
+     * Handler of the starting event.
+     */
+    public void startApp()
+    {
+        // Get main display
+        display = Display.getDisplay(this);
+
+        // Initialize different displayables
+        pianoCanvas = new PianoCanvas();
+        aboutForm = new AboutForm();
+
+        // Add commands to appropriate forms
+        pianoCanvas.addCommand(exit);
+        pianoCanvas.addCommand(about);
+        aboutForm.addCommand(back);
+
+        // Set command listeners
+        pianoCanvas.setCommandListener(this);
+        aboutForm.setCommandListener(this);
+
+        // Set main canvas
+        display.setCurrent(pianoCanvas);
+    }
+
+    /**
+     * Handler of the pausing event.
+     */
+    public void pauseApp()
+    {
+    }
+
+    /**
+     * Handler of the destroying event.
+     *
+     * @param unconditional Flag to indicate whether destroying is unconditional
+     */
+    public void destroyApp(boolean unconditional)
+    {
+    }
+
+    /**
+     * Command handler.
+     *
+     * @param c Command executed
+     * @param d Widget the command was executed on
+     */
+    public void commandAction(Command c, Displayable d)
+    {
+        // Handle exit command
+        if(c == exit && d == pianoCanvas)
+        {
+            destroyApp(false);
+            notifyDestroyed();
+        }
+
+        // Handle about command
+        else if(c == about && d == pianoCanvas)
+        {
+            display.setCurrent(aboutForm);
+        }
+
+        // Handle back command in the about form
+        else if(c == back && d == aboutForm)
+        {
+            display.setCurrent(pianoCanvas);
+        }
+    }
+}
