98fd6475f45f169873da8415dbff8870df3dce34
Wincent Balin <wincent.balin@gmail.com>
2010-06-24 05:31:02 +0200

    Reorganized Help form

--- src/piano/HelpForm.java
+++ src/piano/HelpForm.java
@@ -12,17 +12,26 @@ import javax.microedition.lcdui.StringItem;
 public class HelpForm extends Form
 {
     public static final String HELP_TITLE = "Help";
-    public static final String HELP_TEXT =
-            "This is a simple music instrument.\n" +
-            "Use keypad to play it. The helper under the keyboard " +
-            "shows corresponding buttons and keys.\n" +
+
+    public static final String HELP_SECTION_INTRODUCTION_TITLE = "";
+    public static final String HELP_SECTION_INTRODUCTION_TEXT =
+            "This is a simple music instrument.";
+    public static final String HELP_SECTION_PLAYING_TITLE = "Playing keyboard\n";
+    public static final String HELP_SECTION_PLAYING_TEXT =
+            "Use keypad to play the keyboard. The helper under the keys " +
+            "shows buttons on the keypad and corresponding  keys.\n" +
             "It is possible that you will not be able " +
-            "to use more than one or two keys at once.\n" +
-            "Use arrow keys to change the octave.\n" +
-            "\n" +
-            "Copyright (C) 2010 Wincent Balin\n" +
-            "\n" +
-            "Many thanks to my beta testers Birgit and Marina.\n";
+            "to use more than one or two keys at once " +
+            "due to the limitations of the keypad.\n";
+    public static final String HELP_SECTION_OCTAVE_TITLE = "Changing octave\n";
+    public static final String HELP_SECTION_OCTAVE_TEXT =
+            "Use arrow keys to change the octave.";
+    public static final String HELP_SECTION_COPYRIGHT_TITLE = "Copyright\n";
+    public static final String HELP_SECTION_COPYRIGHT_TEXT =
+            "Copyright (C) 2010 Wincent Balin";
+    public static final String HELP_SECTION_THANKS_TITLE = "Thanks\n";
+    public static final String HELP_SECTION_THANKS_TEXT =
+            "Many thanks to my beta testers Birgit and Marina.";
 
     /**
      * Constructor.
@@ -33,6 +42,10 @@ public class HelpForm extends Form
         super(HELP_TITLE);
 
         // Add text with description of the program
-        append(new StringItem(null, HELP_TEXT, StringItem.PLAIN));
+        append(new StringItem(HELP_SECTION_INTRODUCTION_TITLE, HELP_SECTION_INTRODUCTION_TEXT));
+        append(new StringItem(HELP_SECTION_PLAYING_TITLE, HELP_SECTION_PLAYING_TEXT));
+        append(new StringItem(HELP_SECTION_OCTAVE_TITLE, HELP_SECTION_OCTAVE_TEXT));
+        append(new StringItem(HELP_SECTION_COPYRIGHT_TITLE, HELP_SECTION_COPYRIGHT_TEXT));
+        append(new StringItem(HELP_SECTION_THANKS_TITLE, HELP_SECTION_THANKS_TEXT));
     }
 }
